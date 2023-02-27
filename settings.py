7b3a788e0c12601a51eb27b1c9388edcf6bdf290
snvtix <natslep17@onet.pl>
2023-02-27 18:43:13 +0100

    settings

--- settings.py
+++ settings.py
@@ -5,11 +5,11 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
         self.ship_speed = 3
-        self.bullet_speed = 5
+        self.bullet_speed = 3
         self.bullet_width = 5
         self.bullet_height = 15
         self.bullet_color = (60,60,60)
         self.bullets_allowed = 5
-        self.alien_speed = 0.1
+        self.alien_speed = 0.5
         self.fleet_drop_speed = 1
         self.fleet_direction = 1
